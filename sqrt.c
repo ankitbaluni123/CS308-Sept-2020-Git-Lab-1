e6c44339305e6ed3cd7f9fc86eff19ba4e1d068a
Ankit <b18007@students.iitmandi.ac.in>
2020-10-16 02:57:19 +0530

    Added Valid function

--- sqrt.c
+++ sqrt.c
@@ -3,6 +3,13 @@ positive number. */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+bool valid(int n)  {
+	if (n < 0)
+		return false;
+	return true;
+}
+
 // Ankit Baluni -B18007
 int main(int argc, char* argv[]) {
 	
@@ -12,6 +19,12 @@ int main(int argc, char* argv[]) {
 	}
 
 	int input = atoi(argv[1]);
+	if (!valid(input)) {
+		printf("Negative Number\n");
+		printf("End of program. Exiting\n");
+		return (0);
+	}
+
 	printf("Sqrt of %d is %f\n",input,sqrt(input));
 	printf("Added exit message\n");
 	return(0);
